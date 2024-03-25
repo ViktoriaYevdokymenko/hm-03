71fa05ead0171b91269ec75fa365e36f2ae923b6
Viktoria Yevdokymenko <viktoriia.yevdokymenko-contractor@zoominfo.com>
2024-03-25 13:10:28 +0200

    Create Class1.java
    Numbers

---
+++ Class1.java
@@ -0,0 +1,16 @@
+public class Class1 {
+  public static void main(String[] args) {
+    int start = 20;
+    int end = 100;
+    int sum = 0;
+
+    for (int i = start; i <= end; i++) {
+      if (i % 3 == 0) {
+        sum += i;
+      }
+    }
+
+    System.out.println("Сума всіх кратних 3 чисел на проміжку від 20 до 100: " + sum);
+  }
+}
+
